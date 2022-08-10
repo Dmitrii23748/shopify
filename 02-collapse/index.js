31b7ae7e28322623bf0c8e03ccf0d06bc7cb4f3d
Dmitrii23748 <chunosov.dmitrij@bk.ru>
2022-08-10 21:23:55 +0300

    add animate

--- 02-collapse/index.js
+++ 02-collapse/index.js
@@ -2,10 +2,37 @@ const btnVisible = document.querySelector(".collapsible__action--visible");
 const btnHide = document.querySelector(".collapsible__action--hidden");
 const content = document.querySelector(".collapsible__content");
 
+// btnVisible.addEventListener("click", () => {
+//   content.style.opacity = "0";
+// });
+
+// btnHide.addEventListener("click", () => {
+//   content.style.opacity = "1";
+// });
+
+// c Animate 
+const visibleAnimate = [
+  {
+    opacity: "0",
+  },
+];
+
+const hideAnimate = [
+  {
+    opacity: "1",
+  },
+];
+
 btnVisible.addEventListener("click", () => {
-  content.style.opacity = "0";
+  content.animate(visibleAnimate, {
+    duration: 400,
+    fill: "forwards",
+  });
 });
 
 btnHide.addEventListener("click", () => {
-  content.style.opacity = "1";
+  content.animate(hideAnimate, {
+    duration: 400,
+    fill: "forwards",
+  });
 });

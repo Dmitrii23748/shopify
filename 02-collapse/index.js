6615841659a64d59cc750574ee11a7c610f1e350
Dmitrii23748 <chunosov.dmitrij@bk.ru>
2022-08-13 12:57:40 +0300

    fix: add hide span-text button

--- 02-collapse/index.js
+++ 02-collapse/index.js
@@ -2,15 +2,6 @@ const btnVisible = document.querySelector(".collapsible__action--visible");
 const btnHide = document.querySelector(".collapsible__action--hidden");
 const content = document.querySelector(".collapsible__content");
 
-// btnVisible.addEventListener("click", () => {
-//   content.style.opacity = "0";
-// });
-
-// btnHide.addEventListener("click", () => {
-//   content.style.opacity = "1";
-// });
-
-// c Animate 
 const visibleAnimate = [
   {
     opacity: "0",
@@ -23,16 +14,27 @@ const hideAnimate = [
   },
 ];
 
+const setting = {
+  duration: 400,
+  fill: "forwards",
+};
+
+const loadPage = () => {
+  content.animate(hideAnimate, setting);
+  btnVisible.animate(hideAnimate, setting);
+  btnHide.animate(visibleAnimate, setting);
+};
+
+document.addEventListener("DOMContentLoaded", loadPage);
+
 btnVisible.addEventListener("click", () => {
-  content.animate(visibleAnimate, {
-    duration: 400,
-    fill: "forwards",
-  });
+  content.animate(visibleAnimate, setting);
+  btnVisible.animate(visibleAnimate, setting);
+  btnHide.animate(hideAnimate, setting);
 });
 
 btnHide.addEventListener("click", () => {
-  content.animate(hideAnimate, {
-    duration: 400,
-    fill: "forwards",
-  });
+  content.animate(hideAnimate, setting);
+  btnVisible.animate(hideAnimate, setting);
+  btnHide.animate(visibleAnimate, setting);
 });
